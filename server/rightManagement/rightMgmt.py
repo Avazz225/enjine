53777500e7cc4aab5ed12d1118c30fbd7f8a4e31
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-05-29 09:55:02 +0200

    added docs, now returns more complete error messages

--- server/rightManagement/rightMgmt.py
+++ server/rightManagement/rightMgmt.py
@@ -2,9 +2,15 @@ from database import db_connector
 import helpers
 
 def approveUserManagement(token:str): 
+    """Checks whether a user is allowed to manage users. 
+    
+    Allowed groups:
+    - sysAdmin
+    - userAdmin"""
+
     id = helpers.getID(token)
-    if type(id) != int: return id
+    if type(id) != int: return {'id':'Invalid token'}
 
     row = db_connector.read('permission', ['sysAdmin', 'userAdmin'], {'id': id}, 'one')
     if bool(row['sysAdmin']) or bool(row['userAdmin']): return True
-    else: return id
+    else: return {'id':id, 'rights':helpers.getRights(id)}
