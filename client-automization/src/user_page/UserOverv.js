9181ee03a4af864c2a1cbda9bf6a2ce0361cee74
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-06-05 20:21:25 +0200

    added userManagement

---
+++ client-automization/src/user_page/UserOverv.js
@@ -0,0 +1,202 @@
+import React from 'react'
+import { setLocal, getCookie } from '../helpers';
+import { Icon } from '@iconify/react';
+import lockReset from '@iconify/icons-mdi/lock-reset';
+import accountDetails from '@iconify/icons-mdi/account-details';
+import Pagy from '../components/Pagy';
+import { BtnClass2, BtnClass3 } from '../components/Btn';
+
+function ConfirmationPopUp(props){
+    return(
+        <div className={(props.popUpVisible)?'popUp centered heightCenter visible':'popUp centered heightCenter'}>
+            Passwort für {props.ident} wirklich zurücksetzen?<br/><br/>
+            <BtnClass2 text='Ja, zurücksetzen' action={props.forcePwdReset}/>
+            <BtnClass3 text='Nein, nicht zurücksetzen' action={props.togglePopUpVisiblility}/>
+        </div>
+    )
+}
+
+function ResetPwd(props) {
+    return(
+        <td>
+            <div className='flexWrapper horCenter'>
+                {(props.data['active_account'] === 1)? 
+                    <button className='btn Class3 zeroTB' onClick={() => props.togglePopUpVisiblility(props.data['id'], props.data['identifier'])}>
+                        <div className="iconWrapper">
+                            <Icon icon={lockReset} width={24}/>
+                        </div>
+                    </button>
+                    : 
+                    <></>
+                }
+                <a className='href Class3 zeroTB' target='blank' href={'/accountDetails?aid='+props.data['id']}>
+                    <div className="iconWrapper">
+                        <Icon icon={accountDetails} width={24}/>
+                    </div>
+                </a>
+            </div>
+        </td>
+    )
+}
+
+const TableElement = ({data, togglePopUpVisiblility}) =>( 
+    <>
+    {data.map(data =>(
+        <tr key={data['id']}>
+            <td>{data['identifier']}</td>
+            <td>{(data['active_account'] === 1)?'Aktiv':'Passiv'}</td>
+            <ResetPwd data={data} togglePopUpVisiblility={togglePopUpVisiblility} />
+        </tr>
+    ))}
+    </>
+)
+
+
+class UserTable extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          userData: [{id: '0', identifier: '___', active_account: 1}],
+          serverErrorMessage: '',
+          total: 0,
+          currentPage: new URLSearchParams(window.location.search).get('s'),
+          elementsPerPage: 30,
+          selectedID: 0,
+          selectedUser: '',
+          popUpVisible: false
+        };
+
+        this.forcePwdReset = this.forcePwdReset.bind(this)
+        this.getItems = this.getItems.bind(this)
+        this.setPage = this.setPage.bind(this)
+        this.togglePopUpVisiblility = this.togglePopUpVisiblility.bind(this)
+    }
+
+    // Send the GET request to the backend after mount of component
+    async componentDidMount(){
+        await this.getItems(this.state.currentPage)
+    }
+
+    async forcePwdReset(){
+        // Create an object with the required data
+        const credentials = {
+            id: this.state.selectedID,
+            admReset: 'true'
+        };
+    
+        // Send the POST request to the backend
+        fetch('http://127.0.0.1:5000/changepw', {
+            method: 'UPDATE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth-Header': getCookie('token')
+                },
+                body: JSON.stringify(credentials)
+            })
+            .then((response) => {
+                    // Check the response status
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error(response.status, response.json());
+                    }
+                }
+            )
+            .then((data) => {
+                // Display information
+                    window.alert("Passwort erfolgreich zurückgesetzt! Das neue temporäre Passwort ist: "+data['password'])
+                    this.togglePopUpVisiblility()
+                }
+            )
+            .catch((error, data) => {
+                if (error.message === '401') {
+                    // Insufficient rights
+                    setLocal('userRights', data['rights'])
+                } else {
+                    // Internal server error
+                    window.alert('Datenbankfehler, bitte kontaktiere den zuständigen Administrierenden!');
+                }
+            }
+        );
+    }
+
+    async getItems(page){
+        await fetch('http://127.0.0.1:5000/getUsers', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            'Auth-Header': getCookie('token')
+            },
+            body: JSON.stringify({rowCount:this.state.elementsPerPage, start:(0 + ((parseInt(page)-1) * this.state.elementsPerPage))})
+        })
+        .then((response) => {
+            // Check the response status
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error(response.status, response.json());
+            }
+        })
+        .then((data) => {
+            // Handle the response
+            this.setState({
+                userData: data['res'],
+                total: data['total']
+            })
+        })
+        .catch((error, data) => {
+            if (error.message === '401') {
+                // Unauthorized
+                this.setState({serverErrorMessage: 'Unzureichende Rechte.'})
+                setLocal('userRights', data['rights'])
+            } else {
+                // Internal server error
+                this.setState({serverErrorMessage: 'Datenbankfehler, bitte kontaktiere den zuständigen Administrierenden!'})
+            }
+        });
+    }
+
+    async setPage(number){
+        await this.getItems(number)
+        this.setState({
+            currentPage: number,
+        })
+        let stateObj = { id: "100" };
+        window.history.replaceState(stateObj,'ENJine','/usermgmt?s='+number)
+    }
+
+    togglePopUpVisiblility(selectedID=0, selectedUser=''){
+        this.setState({
+            selectedID: selectedID,
+            selectedUser: selectedUser,
+            popUpVisible: !this.state.popUpVisible
+        })
+    }
+
+    render(){
+        if (this.state.total === 0) return 
+        return(
+            <div className="content">
+                <ConfirmationPopUp forcePwdReset={this.forcePwdReset} ident={this.state.selectedUser} togglePopUpVisiblility={this.togglePopUpVisiblility} popUpVisible={this.state.popUpVisible} />
+                <h1 className='blue noTopSpace'>Nutzerverwaltung</h1>
+                <div className='centered'>
+                    <Pagy currentPage={this.state.currentPage} elementCount={this.state.total} elementPerPage={this.state.elementsPerPage} setPage={this.setPage} />
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Anmeldename</th>
+                                <th>Accounttyp</th>
+                                <th>Optionen</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <TableElement data={this.state.userData} togglePopUpVisiblility={this.togglePopUpVisiblility}/>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserTable
