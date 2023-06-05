53b5c61f20a99cc8d4cd69895c772cbbc7770367
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-06-05 20:21:03 +0200

    Update NotFound.js

--- client-automization/src/not_found/NotFound.js
+++ client-automization/src/not_found/NotFound.js
@@ -3,12 +3,14 @@ import BackgroundLogo from "../components/BackgroundLogo"
 
 function NotFound(){
     return(
-        <div className='flexWrapper heightCenter widthMax'>
+        <div className='centered heightCenter content widthMax'>
             <BackgroundLogo/>
             <div className="centered">
-                <h1 className="blue">Die angeforderte Seite wurde nicht gefunden</h1>
-                Bitte kehre zum Dashboard zurück.<br/><br/>
-                <HrefClass1 text="Zum Dashboard" noUnderline={true} action='/'/>
+                <h1 className="blue noTopSpace">Es tut uns leid, aber die gesuchte Seite existiert nicht.</h1>
+                <h2>Error 404: Maxim not found</h2>
+                Bitte klicke unten, um zum Dashboard zurückzukehren.<br/><br/>
+                <HrefClass1 text="Zum Dashboard" noUnderline={true} action='/'/><br/><br/>
+                Wenn die Seite existieren sollte, wende dich bitte an einen zuständigen Administrierenden.
             </div>
         </div>
     )
