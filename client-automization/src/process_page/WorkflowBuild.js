968d27159a38de1f03d39ef1d038891d559072bb
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-06-12 08:23:45 +0200

    Update WorkflowBuild.js

--- client-automization/src/process_page/WorkflowBuild.js
+++ client-automization/src/process_page/WorkflowBuild.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
-import {DecisionComp, DecisionUniterComp, DraggableComp, EndPoint, StartPoint, InterruptComp} from './DraggableComponents';
-import { BtnClass2 } from '../components/Btn';
+import React from 'react';
+import {DecisionComp, ParallelComp, DraggableComp, EndPoint, StartPoint, InterruptComp} from './DraggableComponents';
+import { BtnClass1, BtnClass2 } from '../components/Btn';
 import Xarrow from "react-xarrows";
 import Sticker from './Sticker';
-import { timeout } from '../helpers';
+import { getObjectById, timeout } from '../helpers';
+import { Icon } from '@iconify/react';
+import closeThick from '@iconify/icons-mdi/close-thick';
 import { getCookie, setLocal } from '../helpers';
 
 function TempArrow(props){
@@ -18,12 +20,12 @@ class WorkflowBuilder extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            dragObjects: [{id:0, name:'Start', type:'start', relatedPlugins: []}, {id:1, name:'End', type:'end', relatedPlugins: []}], 
-            objectPositions: [{id:0,x:0,y:0}, {id:1,x:0,y:0}],
-            connections: [],
+            dragObjects: this.props.dragObjects, 
+            objectPositions: this.props.objectPositions,
+            connections: this.props.connections,
             drag: false,
             previous: '',
-            pluginAssignment: [{}],
+            pluginAssignment:'',
         };
 
         this.abortArrow = this.abortArrow.bind(this);
@@ -73,6 +75,47 @@ class WorkflowBuilder extends React.Component{
         })
     }
 
+    deleteArrow(e){
+        let id = -1
+        try{
+            id = e.target.attributes.name.value
+        } catch {
+            try{
+                id = e.target.parentElement.attributes.name.value
+            }catch{
+                id = e.target.parentElement.parentElement.attributes.name.value
+            }
+           
+        }
+
+        let targetNodes= {}
+
+        let temp = this.state.connections
+        for (var i = 0; i < temp.length; i++) {
+            if (Number(temp[i].id) === Number(id)) {
+                targetNodes = temp[i]
+                temp.splice(i, 1);
+              break;
+            }
+        }
+
+        let tempNodes = this.state.dragObjects
+
+        for (var i = 0; i < tempNodes.length; i++) {
+            if (Number(tempNodes[i]['id']) === Number(targetNodes['previous'])) {
+                tempNodes[i]['outboundConn'] = Number(tempNodes[i]['outboundConn'])-1;
+            } else if (Number(tempNodes[i]['id']) === Number(targetNodes['next'])){
+                tempNodes[i]['inboundConn'] = Number(tempNodes[i]['inboundConn'])-1;
+            }
+        }
+
+        this.setState({
+            connections: temp,
+            dragObjects: tempNodes,
+        })
+
+    }
+
     handleArrowDrag = (e) =>{
         this.setState({
           drag: true, 
@@ -89,21 +132,45 @@ class WorkflowBuilder extends React.Component{
     }
     
     handleArrowFinish = (e) =>{
-        try{
-        let next = e.target.attributes.name.value
-        let temp = this.state.connections
+    	if (this.state.previous !== ''){
+            try{
+                let next = e.target.attributes.name.value
+                let temp = this.state.connections
+                let id = 0
+                if (temp.length !== 0){
+                    id = Number(temp[temp.length-1].id) + 1
+                }
 
-        if (next !== '' && this.state.previous !== next && this.state.previous!== ''){
-            temp.push({previous: this.state.previous, next: next})
-        }
+                if (next !== '' && this.state.previous !== next && this.state.previous!== ''){
+                    temp.push({id: id, previous: this.state.previous, next: next})
+                
 
-        this.setState({
-            drag: false,
-            previous: '',
-            connections: temp,
-        })}
-        catch{
+                    let tempStates = this.state.dragObjects
+                    for (var i = 0; i < tempStates.length; i++) {
+                        if (Number(tempStates[i]['id']) === Number(this.state.previous)) {
+                            tempStates[i]['outboundConn'] = Number(tempStates[i]['outboundConn'])+1;
+                        } else if (Number(tempStates[i]['id']) === Number(next)){
+                            tempStates[i]['inboundConn'] = Number(tempStates[i]['inboundConn'])+1;
+                        }
+                    }
 
+                    this.setState({
+                        drag: false,
+                        previous: '',
+                        connections: temp,
+                        dragObjects: tempStates,
+                    })
+                
+                } else {
+                    this.setState({
+                        drag: false,
+                        previous: '',
+                    })
+                }
+
+            }catch{
+
+            }
         }
     }
 
@@ -130,49 +197,102 @@ class WorkflowBuilder extends React.Component{
     onAddComponent = (e) => {
         let temp = this.state.dragObjects
         let objectPositions = this.state.objectPositions
-        temp.push({id: this.state.dragObjects.length, name:'Task', type: e.target.name, relatedPlugins: []})
-        objectPositions.push({id:this.state.dragObjects.length, x: 0, y:0})
+
+        objectPositions.push({id:(Number(temp[temp.length - 1].id) + 1), x: 0, y:0})
+        temp.push({id: (Number(temp[temp.length - 1].id) + 1) , name:'Task', type: e.target.name, relatedPlugins: [], inboundConn: 0, outboundConn: 0})
         this.setState({
             dragObjects: temp,
             objectPositions: objectPositions,
         })
     }
 
+    saveProcess = () => {
+
+        let data={
+            processName: this.props.task,
+            processObjects: this.state.dragObjects, 
+            objectPositions: this.state.objectPositions,
+            objectConnections: this.state.connections,
+        }
+
+        fetch('http://127.0.0.1:5000/updateProcess', {
+            method: 'UPDATE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Auth-Header': getCookie('token')
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => {
+            // Check the response status
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.status, response.json());
+            }
+        })
+        .then((data) => {
+            // Handle the response
+            this.setState({
+                pluginAssignment: data['program_plugin'],
+            })
+        })
+        .catch((error, data) => {
+            if (error.message === '401') {
+                // Unauthorized
+                this.setState({serverErrorMessage: 'Unzureichende Rechte.'})
+                setLocal('userRights', data['rights'])
+            } else {
+                // Internal server error
+                this.setState({serverErrorMessage: 'Datenbankfehler, bitte kontaktiere den zuständigen Administrierenden!'})
+            }
+        });
+    }
+
     render (){
         return(
-            <>
-                <div><br/><br/>
-                    <BtnClass2 text="Neuen Task hinzufügen" name='task' action={this.onAddComponent}/><br/>
-                    <BtnClass2 text="Neues exklusives Gateway" name='decision' action={this.onAddComponent}/><br/>
-                    <BtnClass2 text="Neues paralleles Gateway" name='parallel' action={this.onAddComponent}/><br/>
-                    <BtnClass2 text="Neuer Wartepunkt" name='interrupt' action={this.onAddComponent}/>
-                    {(this.state.drag)?<TempArrow start={this.state.previous} abortArrow={this.abortArrow}/>:<></>}
-                </div>
-                {this.state.connections.map((item) => (
-                        <>
-                            <Xarrow
-                                start={String(item['previous'])}
-                                end={String(item['next'])}
-                                zIndex={100}
-                                color="rgb(100,100,100)"
-                                curveness={0.5}
-                                strokeWidth={3}
-                            />
-                        </>
-                ))}
-                <div className="dragContainer">
-                    <StartPoint onStop= {this.onStop} name={0} handleArrowDrag={this.handleArrowDrag} /> 
-                    {this.state.dragObjects.map((item) => (
-                        <>
-                        {(item.type==='task')?<DraggableComp relatedPlugins={item.relatedPlugins} handleAppPluginToTask={this.handleAppPluginToTask} name={item['id']} onStop= {this.onStop} text={item['name']} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish} handleTextChange={this.handleTextChange} pluginAssignment={this.state.pluginAssignment} />:
-                        (item.type === 'decision')? <DecisionComp name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>:
-                        (item.type === 'parallel')? <DecisionUniterComp name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>:
-                        (item.type === 'interrupt')? <InterruptComp name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>: <></>
-                        }
-                      </>
-                    ))}
-                    <EndPoint onStop= {this.onStop} name={1} handleArrowFinish={this.handleArrowFinish}/> 
-                </div>
+            <> 
+                {(this.state.pluginAssignment === '')?<></> :
+                    <div className='flexWrapper widthMax row'>
+                        <div><br/>
+                            <h1 className='blue noTopSpace'>{this.props.task}</h1>
+                            <center>
+                                <BtnClass2 text="Neuen Task hinzufügen" name='task' action={this.onAddComponent}/><br/>
+                                <BtnClass2 text="Neues exklusives Gateway" name='decision' action={this.onAddComponent}/><br/>
+                                <BtnClass2 text="Neues paralleles Gateway" name='parallel' action={this.onAddComponent}/><br/>
+                                <BtnClass2 text="Neuer Wartepunkt" name='interrupt' action={this.onAddComponent}/><br/><br/>
+                                <BtnClass1 text="Prozess speichern" action={this.saveProcess} />
+                            </center>
+                            {(this.state.drag)?<TempArrow start={this.state.previous} abortArrow={this.abortArrow}/>:<></>}
+                        </div>
+                        {this.state.connections.map((item) => (
+                                <>
+                                    <Xarrow
+                                        start={String(item['previous'])}
+                                        end={String(item['next'])}
+                                        zIndex={100}
+                                        color="rgb(100,100,100)"
+                                        curveness={0.5}
+                                        strokeWidth={3}
+                                        labels={<div className='error arrowLabel' name={item['id']} onClick={(e) => this.deleteArrow(e)}><Icon icon={closeThick}/></div>}
+                                    />
+                                </>
+                        ))}
+                        <div className="dragContainer">
+                            <StartPoint outboundConn={this.state.dragObjects[0].outboundConn} maxOutboundConn={1} onStop= {this.onStop} name={0} handleArrowDrag={this.handleArrowDrag} objectPosition={this.state.objectPositions[0]} /> 
+                            {this.state.dragObjects.map((item) => (
+                                <>
+                                {(item.type==='task')?<DraggableComp inboundConn={item.inboundConn} outboundConn={item.outboundConn} maxInboundConn={1} maxOutboundConn={1} objectPosition={getObjectById(this.state.objectPositions, item['id'])} relatedPlugins={item.relatedPlugins} handleAppPluginToTask={this.handleAppPluginToTask} name={item['id']} onStop= {this.onStop} text={item['name']} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish} handleTextChange={this.handleTextChange} pluginAssignment={this.state.pluginAssignment} />:
+                                (item.type === 'decision')? <DecisionComp inboundConn={item.inboundConn} outboundConn={item.outboundConn} maxInboundConn={1} maxOutboundConn={2} objectPosition={getObjectById(this.state.objectPositions, item['id'])} name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>:
+                                (item.type === 'parallel')? <ParallelComp inboundConn={item.inboundConn} outboundConn={item.outboundConn} maxInboundConn={2} maxOutboundConn={2} maxConn={3} objectPosition={getObjectById(this.state.objectPositions, item['id'])} name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>:
+                                (item.type === 'interrupt')? <InterruptComp inboundConn={item.inboundConn} outboundConn={item.outboundConn} maxInboundConn={1} maxOutboundConn={1} objectPosition={getObjectById(this.state.objectPositions, item['id'])} name={item['id']} onStop= {this.onStop} handleArrowDrag={this.handleArrowDrag} handleArrowFinish={this.handleArrowFinish}/>: <></>
+                                }
+                            </>
+                            ))}
+                            <EndPoint inboundConn={this.state.dragObjects[1].inboundConn}  maxInboundConn={1} onStop= {this.onStop} name={1} handleArrowFinish={this.handleArrowFinish} objectPosition={this.state.objectPositions[1]}/> 
+                        </div>
+                    </div>
+                }
             </>
         )
     }
