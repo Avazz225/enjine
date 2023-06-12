35cc3b1b49434aab1d88da41f5dd0e6ad955d10d
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-06-12 08:23:59 +0200

    example Plugin

---
+++ server/functionality/function_plugin/gmail.py
@@ -0,0 +1,8 @@
+def getParams():
+    return{
+        'recipient': 'Der Empfänger der Email.',
+        'CC-recipient': 'Ein Nutzer der im CC erwähnt wird.',
+        'topic': 'Betreff der Email.',
+        'text': 'Der Text der Email.',
+        'replacementValues': 'Ersetzt Werte aus dem Emailtext.',
+    }
