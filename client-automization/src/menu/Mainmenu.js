0208a6703f229f9b60d4661f4089fd820b662e64
Avazz225 <84507493+Avazz225@users.noreply.github.com>
2023-06-13 08:22:29 +0200

    alterations for model B

--- client-automization/src/menu/Mainmenu.js
+++ client-automization/src/menu/Mainmenu.js
@@ -1,4 +1,4 @@
-import { HrefClass1, HrefClass3 } from "../components/Href"
+import { HrefClass1, HrefClass3, HrefClass2 } from "../components/Href"
 import { getLocal, logout } from "../helpers"
 import { Icon } from '@iconify/react';
 import logoutIcon from '@iconify/icons-mdi/logout';
@@ -13,17 +13,18 @@ function Mainmenu(){
         </a>
         <div className="menuBackground mainMenu">
             <div className="mainSlave">
-                <HrefClass3 text="Dashboard" noUnderline={true} action='/'/>
-
+                
+                {(perm['sysAdmin']===1|perm["processAdmin"]===1|perm["processGlobal"]===1)?<HrefClass3 text="Prozessverwaltung" noUnderline={true} action='/processmgmt'/>:<></>}
                 {(perm['sysAdmin']===1|perm["userAdmin"]===1|perm["userGlobal"]===1)?<HrefClass3 text="Nutzerverwaltung" noUnderline={true} action='/usermgmt?s=1'/>:<></>}
                 {(perm['sysAdmin']===1|perm["processAdmin"]===1|perm["processGlobal"]===1)?<HrefClass3 text="Aktive Prozesse" noUnderline={true} action='/activeprocesses'/>:<></>}
-                {(perm['sysAdmin']===1|perm["processAdmin"]===1|perm["processGlobal"]===1)?<HrefClass3 text="Prozessverwaltung" noUnderline={true} action='/processmgmt'/>:<></>}
                 
-                {(perm['sysAdmin']===1|perm["groupAdmin"]===1|perm["groupGlobal"]===1)?<HrefClass3 text="Gruppenverwaltung" noUnderline={true} action='/groupmgmt'/>:<></>}
+                {(perm['sysAdmin']===1|perm["groupAdmin"]===1|perm["groupGlobal"]===1)?<HrefClass2 text="Gruppenverwaltung" noUnderline={true} action='/groupmgmt'/>:<></>}
                 {(perm['sysAdmin']===1|perm["applicationAdmin"]===1|perm["applicationGlobal"]===1)?<HrefClass3 text="Programmverwaltung" noUnderline={true} action='/programmgmt'/>:<></>}
                 {(perm['sysAdmin']===1|perm["pluginAdmin"]===1)?<HrefClass3 text="Pluginkonfiguration" noUnderline={true} action='/pluginmgmt'/>:<></>}
+                <HrefClass3 text="Dashboard" noUnderline={true} action='/'/>
                 {(perm['sysAdmin']===1|perm["rightAdmin"]===1|perm["rightGlobal"]===1|perm["rightLocal"]===1)?<HrefClass3 text="Rechteverwaltung" noUnderline={true} action='/rightmgmt'/>:<></>}
                 {(perm['sysAdmin']===1|perm["logAdmin"]===1|perm["logGlobal"]===1)?<HrefClass3 text="Logs" noUnderline={true} action='/logs'/>:<></>}
+                
             </div>
             <LogOut/>  
         </div>
